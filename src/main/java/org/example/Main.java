2f27cc3a75300da37381da1e29a0ac6364f8b0f1
Vsvolod <110387504+Vsvolod@users.noreply.github.com>
2022-08-22 01:45:24 +0300

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,35 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        writeToJson();
     }
-}
+    public static ArrayList<User> readFile() throws Exception {
+        ArrayList <User> usersFields = new ArrayList<>();
+        FileReader fr = new FileReader("task2.txt");
+        Scanner sc = new Scanner(fr);
+        sc.nextLine();
+        while (sc.hasNextLine()) {
+                String temp = sc.nextLine();
+                String [] tempAr = temp.split(" ");
+                usersFields.add(new User(tempAr[0], tempAr[1]));
+        }return usersFields;
+    }
+    public static void writeToJson() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File("user.json"), readFile());
+
+        }
+    }
+
+
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,2 +1,12 @@
-package org.example;public class User {
+package org.example;
+
+public class User {
+    public String name;
+    public String age;
+
+    public User(String name, String age){
+        this.name = name;
+        this.age = age;
+    }
+
 }
